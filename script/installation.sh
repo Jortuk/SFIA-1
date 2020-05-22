48c498a47f0ce8782df4fb88e6e0aa5da279e081
Jortuk <Jordan.Taylor@academytrainee.com>
2020-05-22 10:08:36 +0000

    Updated installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -10,11 +10,11 @@ sudo apt install python3-venv -y
 
 python3 -m venv venv
 
+cd /var/lib/jenkins/workspace/sfia1_freestyle
+
 source /var/lib/jenkins/workspace/sfia1_freestyle/venv/bin/activate
 
 pip3 install -r requirements.txt
-
-cd /var/lib/jenkins/workspace/sfia1_freestyle
 
 source ~/.bashrc
 
